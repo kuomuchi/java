187b602fea1ff5692ee06f48ddff7b3b53a38eae
killua kuo <killuakuo@killuade-MacBook-Air.local>
2018-07-05 23:04:01 +0800

    make it better

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ bot.on('message', function (event) {
 	var hello = items[Math.floor(Math.random()*items.length)];
 	var Ha = ["1.餅乾是男的還是女的？","2.水餃是公的還是母的？", "3.為什麼女生不能常常生氣？","4.月經來了，猜一種過年用品","5.四個男生觸電，猜一種物品"];
 	var 髒髒 = Ha[Math.floor(Math.random()*Ha.length)];
+	var 我 = "U813645541c262fb6d9c967efeb884aeb";
+
+
 
 	if (收到 == "笑話答案5") {
 		event.reply("電四雞").then(function (data) {
@@ -165,6 +168,15 @@ bot.on('message', function (event) {
 			console.log('Error', error);
 		});
 	}
+
+	if(event.source.userId === 我){
+	if (收到 == "現在要幹嘛！") {
+		event.reply("打...星海！！！").then(function (data) {
+			console.log('Success', data);
+		}).catch(function (error) {
+			console.log('Error', error);
+		});
+	}}
 
 	if (收到 == "id") {
 		event.reply(event.source.userId).then(function (data) {
