03bde39f57bd7a20994b3593444dc00eb8854606
killua kuo <killuakuo@killuade-MacBook-Air.local>
2018-07-10 13:32:32 +0800

    make it better

--- index.js
+++ index.js
@@ -32,19 +32,6 @@ bot.on('message', function (event) {
 	var attack = ["你到底在玩什麼啊！！只會送頭！！還不把遊戲刪掉！！","廢物！","你到底有沒有腦啊！","還不跪下來叫聲師父！","一群低智商生物..."];
 	var Att = attack[Math.floor(Math.random()*attack.length)];
 
-var qwer = function LOL (mu){
-	test.push(mu);
-}
-
-
-	if(收到== qwer("wtf")){
-		event.reply(test).then(function (data) {
-			console.log('Success', data);
-		}).catch(function (error) {
-			console.log('Error', error);
-		});
-	}
-
 
 	if (收到.toLowerCase() == "wtf"||收到 === "ＷＴＦ") {
 		event.reply("Fuck you").then(function (data) {
