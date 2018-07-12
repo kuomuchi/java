3e372fea67df5f4580c7c595df87bc1b20aaf2eb
killua kuo <killuakuo@killuade-MacBook-Air.local>
2018-07-12 23:10:05 +0800

    make it better

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ if(End == 3||End == 4||End == 5){
 
 
 if(End == 3){
-	if (收到.toLowerCase() == i1) {
+	if (收到.toLowerCase() == "i1") {
 		player1.push(event.source.userId)
 		event.reply("收到").then(function (data) {
 			console.log('Success', data);
@@ -72,7 +72,7 @@ if(End == 3){
 
 
 if(End == 4){
-	if (收到.toLowerCase() == i2) {
+	if (收到.toLowerCase() == "i2") {
 		End = 5;
 		if(event.source.userId != player1){
 		player2.push(event.source.userId)
