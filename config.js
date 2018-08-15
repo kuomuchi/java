3cfff0729d822a8f07346e600c62359b701752a6
kuomuchi <42135910+kuomuchi@users.noreply.github.com>
2018-08-15 10:42:52 +0800

    hide something you never know

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+channelId: "1592045256",
+channelSecret: "0482f74e803017ff1f0d2ccb97fad079",
+channelAccessToken: "uZcmX22GDoNMCQHfJkqErCRQamOgt14kiAGA8lGYXJy+ESNle7wHzXPP6fxz+e0M8wDXZQDLTn89mm15PWb+i5BSC1iYpQPJbjfeZpjV6AxSdiQII1X4v+Cjiv2tczLhj4r1pq4MCKhec6gNq5mpzwdB04t89/1O/w1cDnyilFU="
+}
