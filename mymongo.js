4960eb9626147ea54cee597bc4f3cf4157948bb5
kuomuchi <42135910+kuomuchi@users.noreply.github.com>
2018-08-20 16:03:50 +0800

    plz..

--- mymongo.js
+++ mymongo.js
@@ -1,56 +1,60 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017/data";
 
+module.exports = {
+  insertMongo: function(col, data) {
+    MongoClient.connect (url, function(err, db) {
+      if (err) throw err;
+      db.collection(col, function (err, collection) {
+        if (err) throw err;
+        if (Array.isArray(data)) {
+          collection.insertMany(data);
+        } else {
+          collection.insertOne(data);
+        }
+        db.close();
+      })
+    })
+  },
+  getMongo: function (col, query, func) {
+    MongoClient.connect (url, function(err, db) {
+      if (err) throw err;
+      db.collection(col, function (err, collection) {
+        if (err) throw err;
+        collection.find(query).toArray(function(err, result) {
+          func (result);
+        });
+        db.close();
+      })
+    })
+  },
+  deleteMongo: function(col, query) {
+    MongoClient.connect (url, function(err, db) {
+      if (err) throw err;
+      db.collection(col, function (err, collection) {
+        if (err) throw err;
+        collection.deleteMany(query);
+        db.close();
+      })
+    })
+  },
+  updateMongo: function(col, query, data) {
+    MongoClient.connect (url, function(err, db) {
+      if (err) throw err;
+      db.collection(col, function (err, collection) {
+        if (err) throw err;
+        collection.updateMany(query, value);
+        db.close();
+      })
+    })
+  }
+}
+
+
 //插入資料用 insertMongo(DataBase的連結, Collection的名字, 要輸入的資料)
-export var insertMongo = function(col, data) {
-  MongoClient.connect (url, function(err, db) {
-    if (err) throw err;
-    db.collection(col, function (err, collection) {
-      if (err) throw err;
-      if (Array.isArray(data)) {
-        collection.insertMany(data);
-      } else {
-        collection.insertOne(data);
-      }
-      db.close();
-    })
-  })
-}
 //取得資料用 getMongo(DataBase的連結, Collection名字, 搜尋範圍（無範圍時用"{}"）)
-export var getMongo = function (col, query, func) {
-  MongoClient.connect (url, function(err, db) {
-    if (err) throw err;
-    db.collection(col, function (err, collection) {
-      if (err) throw err;
-      collection.find(query).toArray(function(err, result) {
-        func (result);
-      });
-      db.close();
-    })
-  })
-}
 //刪掉資料 deleteMongo(DataBase的連結, Collection名字, 要刪的資料)
-export var deleteMongo = function(col, query) {
-  MongoClient.connect (url, function(err, db) {
-    if (err) throw err;
-    db.collection(col, function (err, collection) {
-      if (err) throw err;
-      collection.deleteMany(query);
-      db.close();
-    })
-  })
-}
 //更新資料 deleteMongo(DataBase的連結, Collection名字, 要更新的資料, 更新的資料)
-export var updateMongo = function(col, query, data) {
-  MongoClient.connect (url, function(err, db) {
-    if (err) throw err;
-    db.collection(col, function (err, collection) {
-      if (err) throw err;
-      collection.updateMany(query, value);
-      db.close();
-    })
-  })
-}
 //把Collection刪掉 dropMongo(DataBase連結, Collection名字)
 /*
 var dropMongo = function(col) {
