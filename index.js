8a3710eb3b288fa8451811de49aeef0031e55782
kuomuchi <42135910+kuomuchi@users.noreply.github.com>
2018-08-20 16:40:11 +0800

    ?

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ bot.on('message', function (event) {
 	}
 
 	if (/^delete\sid\s\S+/.test(eventText)) {
-		mymongo.deleteMongo("form", [{ _id: eventText.split(" ")[2]}] )
+		mymongo.deleteMongo("form", { "_id": eventText.split(" ")[2]})
 		mymongo.getMongo("form", {}, function(result) {
 			event.reply(JSON.stringify(result))
 		})
